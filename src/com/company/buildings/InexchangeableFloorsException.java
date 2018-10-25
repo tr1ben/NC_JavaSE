60d3fe6c83f00d644cd371033dabd3e9174f14b0
tr1b3n <tr1ben@mail.ru>
2018-10-25 12:57:34 +0400

    add InexchangeableFloorsException & InexchangeableSpacesException

---
+++ src/com/company/buildings/InexchangeableFloorsException.java
@@ -0,0 +1,9 @@
+package com.company.buildings;
+
+public class InexchangeableFloorsException extends Exception {
+    public InexchangeableFloorsException() {}
+
+    public InexchangeableFloorsException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/company/buildings/InexchangeableSpacesException.java
@@ -0,0 +1,9 @@
+package com.company.buildings;
+
+public class InexchangeableSpacesException extends Exception {
+    public InexchangeableSpacesException() {}
+
+    public InexchangeableSpacesException(String message) {
+        super(message);
+    }
+}
